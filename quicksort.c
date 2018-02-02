69466f69e004254c7a32eb52b54ec13b794e45a8
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-02-02 16:48:58 +0530

    Create quicksort.c

---
+++ quicksort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n,a[n],b,k;
+printf("enter the number: ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=1;j<=n;j++)
+{
+for(k=j+1;k<=n;k++)
+{
+if(a[j]>=a[k])
+{
+b=a[j];
+a[j]=a[k];
+a[k]=b;
+}
+}
+}
+printf("%d\t",b);
+}
+
