91fc7ef5feb2db63fb079eb3376c64fdfee0b931
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-02-09 16:59:40 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    int c= 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+    {
+       
+        n =n/ 10;
+        ++c;
+    }
+
+    printf("Number of digits: %d", c);
+    return 0;
+}
