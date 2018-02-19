b11c4d6797a6eebf2d62c9dc01206aa2bb0ba106
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-02-19 16:02:08 +0530

    Create 43a.c

---
+++ 43a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char a[50],b[50];
+printf("enter the character:\n");
+scanf("%s",&a);
+printf("enter the next character:\n");
+scanf("%s",&b);
+printf("%s%s",a,b);
+}
