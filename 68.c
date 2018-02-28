dc35fc9537129a200676f38286ee0284abbdc09d
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-02-28 15:38:55 +0530

    Rename 69.c to 68.c

---
+++ 68.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("enter the number: ");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		if(n%7==0)
+		{
+			printf("%d",n);
+			break;
+		}
+		n++;
+	}
+	return 0;
+}
