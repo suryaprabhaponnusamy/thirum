aae38ed1143a8b8062dabfae174041d597ca70e4
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-03-02 15:57:56 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int i,b,a;
+  char str[10];
+  printf("enter the string");
+  scanf("%s",str);
+  a=strlen(str);
+  b='*';
+  str[a/2]=b;
+  
+  for(i=0;i<a;i++)
+  {
+    printf("%c",str[i]);
+  }
+  return 0;
+}
