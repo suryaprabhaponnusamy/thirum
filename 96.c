4c219ce1c79c04d56e50754c9698d68611993556
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-03-14 16:46:05 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int n,i,flag=0;
+printf("enter the number: "); 
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("\n prime");
+}
+else
+{
+printf("\n composite");
+}
+}
