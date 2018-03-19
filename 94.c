6d5946830c4b7d4a8744a1d1376e3b403099fe32
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-03-19 16:21:17 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,m,a[10],i;
+	printf("enter two number: ");
+	scanf("%d %d",&n,&m);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("%d",a[m-1]);
+	return 0;
+}
